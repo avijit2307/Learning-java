60105499090b82c71d26a6ee3feef9a85732e3b4
avijit2307 <aviappu.mandal@gmail.com>
2022-01-31 10:54:15 +0530

    SwapNumbers file is added to te program

---
+++ SwapNumbers.java
@@ -0,0 +1,11 @@
+public class SwapNumbers {
+	public static void main(String[] args) {
+		int a = 5;
+		int b = 6;
+		int temp = a;
+		a = b;
+		b = temp;
+		System.out.println("a = " + a + " and b = " + b);
+
+	}
+}
